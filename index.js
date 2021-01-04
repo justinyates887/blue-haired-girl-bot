318691e792040db339d08a9884f72c1ab69983e5
Justin Yates <justinyates887@gmail.com>
2021-01-03 16:32:57 -0800

    made changes

--- index.js
+++ index.js
@@ -3,41 +3,43 @@ const fs = require("fs");
 const config = yaml.safeLoad(fs.readFileSync("./config.yml", "utf8"));
 
 
-const env = require('dotenv').config();
+require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.client();
 
 
-
 client.on('ready', () => {
-    console.log('Logged in as ${bot.user.tag} (${bot.user.id}) on ${bot.guilds.size} servers');
-    bot.user.setGame('${cfg.prefix}help | ${bot.guilds.size} servers!');
+    console.log(`Logged in as ${bot.user.tag} (${bot.user.id}) on ${bot.guilds.size} servers`);
+    bot.user.setGame(`${cfg.prefix}help | ${bot.guilds.size} servers!`);
 });
 
 client.on('message', (msg) => {
-    if (msg.author.bot || !msg.content.startsWith(cfg.prefix)) return;
+    if (msg.author.bot || !msg.content.startsWith(prefix)) return;
     const args = msg.content.slice(cfg.prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
+
 //ban command
     if (command === 'ban'){
         if(!msg.member.permissions.has('ADMINISTRATOR')) return msg.reply('you aren\'t an Admin!');
         const member = msg.mentions.members.first();
-        if(!member) return msg.reply('Please mention the member you would like to ban!');
+        if(!member) return msg.reply('please mention the member you would like to ban!');
         msg.channel.send('Please enter a reason');
         const rsn = msg.content;
         member.ban({
             reason: rsn
         });
+        msg.channel.send(`${member} banned sucsesfully!`);
         //need to add reason functionality
     }
 //kick command
     if (command === 'kick'){
         if(!msg.member.permissions.has('ADMINISTRATOR')) return msg.reply('you aren\'t an Admin!');
         const member = msg.mentions.members.first();
-        if(!member) return msg.reply('Please mention the member you would like to ban!');
+        if(!member) return msg.reply('please mention the member you would like to ban!');
         member.kick;
     }
 })
+//mute command
 
 client
     .on('guildCreate', console.log)
