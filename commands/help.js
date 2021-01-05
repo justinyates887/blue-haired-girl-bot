9dfbdd60061eae77d4ce29c748c060a5be551283
Justin Yates <justinyates887@gmail.com>
2021-01-04 19:05:14 -0800

    made minor changes

--- commands/help.js
+++ commands/help.js
@@ -11,7 +11,7 @@ module.exports = {
             let embed = new Discord.MessageEmbed() //sets send card message
                 .setAuthor("Command List") // Header of card
                 .setColor("#486dAA") //Side bar color
-                .setDescription("- **!help** gives list of commands\n- **!kick** kicks selected user *[!<command> + <@user> + <reason (if any)>]*\n- **!ban** bans selected user *[!<command> + <@user> + <reason (if any)>]*\n- **!purge** purges messages *[!<command> + <quantity to delete>]*\n- **!nuke** deletes the channel and creates a replica **(this cannot be undone)**\n") //main text body
+                .setDescription("- **!help** gives list of commands\n- **!kick** kicks selected user *[!<command> + <@user> + <reason (if any)>]*\n- **!ban** bans selected user *[!<command> + <@user> + <reason (if any)>]*\n- **!purge** purges messages *[!<command> + <quantity to delete>]*\n- **!nuke** deletes the channel and creates a replica **(this cannot be undone)**\n- **!channelcreate** creates a channel *[!<command> + <channel type (voice/text)> + <channel name>]*\n") //main text body
                 .setFooter(config.footer) //footer/watermark
                 return msg.channel.send(embed);
         }
